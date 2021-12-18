fc8e00b68854105c5205ea71a4d225bb537b3141
oroumand <ar.oroumand@gmail.com>
2021-12-18 17:52:53 +0330

    02. C# and DotNet\10. ExceptionHandling

---
+++ 02. C# and DotNet/10. ExceptionHandling/ExceptionHandling/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class Calculator
+    {
+        public void Add(int number01, int number02)
+        {
+            if (number01 < 1)
+                throw new ArgumentException();
+            if (number02 < 1)
+                throw new InvalidOperationException();
+
+            int result = number01 + number02;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 02. C# and DotNet/10. ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExceptionHandling;
+
+Console.WriteLine("Hello, World!");
+
+
+Calculator calculator = new Calculator();
+try
+{
+    calculator.Add(1, 2);
+    calculator.Add(0, 3);
+
+}
+catch(InvalidOperationException ex) when (DateTime.Now.Hour == 2)
+{
+
+}
+catch (InvalidOperationException ex) when (DateTime.Now.Hour == 3)
+{
+
+}
+catch (Exception ex) when (ex.InnerException != null)
+{
+    Console.WriteLine(ex.Message);
+    MyCustomException myCustomException = new MyCustomException("My Exception",ex);
+    
+    throw myCustomException;
+
+}
+finally
+{
+    Console.WriteLine("Finally");
+}
