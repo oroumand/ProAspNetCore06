8d4af668c91422e042fa6d9b3a67376d63b27a11
Alireza Oroumand <ar.oroumand@gmail.com>
2023-01-06 17:01:34 +0330

    add signalR samples

---
+++ 06. Framework and Tools/07. SignalR/SignalRSamples/SignalRSamples/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSamples.Hubs;
+
+public class ChatHub:Hub
+{
+    public async Task SendMessage(string userName,string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage",userName,message);
+    }
+}
---
+++ 06. Framework and Tools/07. SignalR/SignalRSamples/SignalRSamples/wwwroot/js/chat.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendMessage").disabled = true;
+
+connection.on("ReceiveMessage", function (userName, message) {
+    var li = document.createElement("li");
+    li.textContent = `${userName}: ${message}`;
+    document.getElementById("messages").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendMessage").disabled = false;
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendMessage").addEventListener("click", function (event) {
+    var userName = document.getElementById("userName").value;
+    var message = document.getElementById("message").value;
+    connection.invoke("SendMessage", userName, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
