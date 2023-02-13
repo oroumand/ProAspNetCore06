6ab161ae143b5c8cf83695508cec6666a82b2a23
Alireza Oroumand <ar.oroumand@gmail.com>
2023-02-13 12:27:59 +0330

    RealTimeWebSamples

---
+++ 06. Framework and Tools/07. SignalR/RealTimeWebSamples/RealTimeWebSamples.WebSocket/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealTimeWebSamples.WebSockets.Models;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace RealTimeWebSamples.WebSockets.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContextAccessor)
+    {
+        _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
+
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+    public async void CountUntil(int id)
+    {
+        var context = _httpContextAccessor.HttpContext;
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            for (int i = 0; i < id; i++)
+            {
+                Thread.Sleep(1000);
+                var jsonMessage = $"\"{i}\"";
+                await webSocket.SendAsync(buffer: new ArraySegment<byte>(
+                        array: Encoding.ASCII.GetBytes(jsonMessage),
+                        offset: 0,
+                        count: jsonMessage.Length),
+                    messageType: WebSocketMessageType.Text,
+                    endOfMessage: true,
+                    cancellationToken: CancellationToken.None);
+            }
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                "Done", CancellationToken.None);
+        }
+        else
+        {
+            context.Response.StatusCode = 400;
+        }
+    }
+}
+
---
+++ 06. Framework and Tools/07. SignalR/RealTimeWebSamples/RealTimeWebSamples.WebSocket/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+var webSocketOptions = new WebSocketOptions()
+{
+    KeepAliveInterval = TimeSpan.FromSeconds(120),
+    ReceiveBufferSize = 4_096//4KB
+};
+app.UseWebSockets(webSocketOptions);
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
