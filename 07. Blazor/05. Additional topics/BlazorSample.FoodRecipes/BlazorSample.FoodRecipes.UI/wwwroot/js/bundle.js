b0411b8debb0bc47038d6d1b1b6fb2ab871cc962
Alireza Oroumand <ar.oroumand@gmail.com>
2023-02-18 19:26:21 +0330

    UI UX

---
+++ 07. Blazor/05. Additional topics/BlazorSample.FoodRecipes/BlazorSample.FoodRecipes.UI/wwwroot/js/bundle.js
@@ -0,0 +1,1 @@
+window.addEventListener("scroll",(function(){window.scrollY>100?(document.querySelector(".navbar").classList.remove("navbar-scroll"),document.querySelector(".navbar").classList.add("navbar-scroll")):(document.querySelector(".navbar").classList.remove("navbar-scroll"),document.querySelector(".section-3").classList.add("navbar-scroll"))}));
